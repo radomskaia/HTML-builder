4bdab3cd100425f4c2d65fdc2f984b27fe57ba39
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-01-20 17:20:23 +0700

    feat: implement 03-files-in-folder module

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const { readdir, stat } = require('fs/promises');
+const path = require('path');
+
+async function getFiles(dir, file) {
+  try {
+    const files = await readdir(path.join(dir, file), {
+      withFileTypes: true,
+    });
+    for (const file of files) {
+      if (file.isDirectory()) {
+        return;
+      }
+      const filePath = path.join(file.parentPath, file.name);
+      const fileStat = await stat(filePath);
+      const fileExtension = path.extname(filePath);
+      const fileName = path.basename(filePath, fileExtension);
+      console.log(`${fileName} - ${fileExtension} - ${fileStat.size}b`);
+    }
+  } catch (err) {
+    console.log('Something went wrong', err);
+  }
+}
+
+getFiles(__dirname, 'secret-folder');
