19539ccdda654988ad022259dc78c3bc565e8a24
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-01-20 15:04:17 +0700

    feat: implement 01-read-file module

---
+++ 01-read-file/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const readStream = fs.createReadStream(path.join(__dirname, './text.txt'));
+
+readStream.on('data', (chunk) => {
+  console.log(chunk.toString());
+});
+readStream.on('end', () => {
+  console.log('The file has been readed');
+  process.exit();
+});
+readStream.on('error', (err) => {
+  console.log('Something went wrong', err);
+});
